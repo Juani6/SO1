3154b32e15654daf37e06ae62ef262e8b5c11ae0
Juani6 <juani.cicerchia@gmail.com>
2025-03-15 15:00:15 -0300

    limpieza

--- U1/shell.c
+++ U1/shell.c
@@ -8,65 +8,41 @@
 #include <string.h>
 #define MAX 1024
 
-
-
-char* concatenar(char* str1, char* str2) {
-    int len1 = strlen(str1);
-    int len = (len1 + strlen(str2)) - 1;
-    char* res = malloc(sizeof(char) * len);
-    
-    for (int i = 0; i < len; i++) {
-        if (i < len1)
-            res[i] = str1[i];
-        else
-            res[i] = str2[i - len1];
-    }
-    return res;
-}
-
-
-
 int main() {
+    int status;
     char buff[MAX], *token;
     char* tokensArr[MAX];
-    int status;
-    int cerrar = 0;
-    const char* exitcmd = "exit";
+    char path[] = "/bin/";
+    const char exitcmd[] = "exit";
 
+    printf("> ");
     fgets(buff,MAX,stdin);
-    
-    while(cerrar == 0) {
+
+    while(1) {
+        buff[strlen(buff)-1] = '\0';
         
-        if (strcmp(buff,exitcmd) == 0)
-            cerrar = 1;
-        
+        if(strcmp(buff,exitcmd) == 0) {
+            return 0;
+        }
 
         __pid_t pid = fork();
         if (pid == 0) {
-            buff[strlen(buff)-1] = '\0';
-
             int i = 0;
             for(token = strtok(buff," "); token != NULL; i++) {
                 tokensArr[i] = token;
                 token = strtok(NULL," ");
             }
             tokensArr[i] = NULL;
-
-            int error = execv(tokensArr[0], tokensArr);
             
-            if (error == -1) {
-                printf("\nError\n");
+            strcat(path,tokensArr[0]);
+            int error = execv(path, tokensArr);
+            if (error == -1)
                 exit(0);
-            }              
         }
         else {
             wait(&status);
+            printf("> ");
             fgets(buff,MAX,stdin);
-            printf("Buffer : %s\n", buff);
-
-            if (strcmp(buff,exitcmd) == 0) {
-                cerrar = 1;
-            }
         }
     }
     return 0;
