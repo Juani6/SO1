011dc3ad12d628f31bda199032b25bc808f37724
Juani6 <juani.cicerchia@gmail.com>
2025-03-15 21:20:29 -0300

    shellfinal?

--- U1/shell.c
+++ U1/shell.c
@@ -1,32 +1,30 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 1024
+#define MAX 128
 
 int main() {
     int status;
-    char buff[MAX], *token;
-    char* tokensArr[MAX];
-    char path[] = "/bin/";
-    const char exitcmd[] = "exit";
-
-    printf("> ");
-    fgets(buff,MAX,stdin);
+    char buff[MAX], *token, *tokensArr[MAX];
+    char path[MAX + 5] = "/bin/";
+    const char EXITCMD[] = "exit";
 
     while(1) {
+        
+        printf("> ");
+        fgets(buff,MAX,stdin);
         buff[strlen(buff)-1] = '\0';
         
-        if(strcmp(buff,exitcmd) == 0) {
+        if(strcmp(buff,EXITCMD) == 0) 
             return 0;
-        }
-
+        
         __pid_t pid = fork();
         if (pid == 0) {
+            if (strlen(buff) == 0)
+                exit(0);
+            
             int i = 0;
             for(token = strtok(buff," "); token != NULL; i++) {
                 tokensArr[i] = token;
@@ -36,13 +34,12 @@ int main() {
             
             strcat(path,tokensArr[0]);
             int error = execv(path, tokensArr);
+            
             if (error == -1)
                 exit(0);
         }
         else {
             wait(&status);
-            printf("> ");
-            fgets(buff,MAX,stdin);
         }
     }
     return 0;
