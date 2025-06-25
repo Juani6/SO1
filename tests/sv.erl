0526a5d37902c0da65f45a7a281620cda5f43d88
Juani6 <juani.cicerchia@gmail.com>
2025-06-24 22:48:12 -0300

    envio el tamanio del archivo

--- tests/sv.erl
+++ tests/sv.erl
@@ -1,7 +1,7 @@
 -module(sv).
 -export([recibir/0, enviar/3]).
 
--define(PORT, 8080).
+-define(PORT, 5050).
 
 recibir() ->
 	{ok, Lsock} = gen_tcp:listen(?PORT, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]),
@@ -10,20 +10,39 @@ recibir() ->
 	io:format("Conexion aceptada~n"),
 	recibir_archivo(Sock),
 	gen_tcp:close(Sock),
-	gen_tco:close(Lsock).
+	gen_tcp:close(Lsock).
+
+% file:write_file("archivo_recibido", Bin),
 
 recibir_archivo(Sock) -> 
-	case gen_tcp:recv(Sock,0) of
-		{ok, Bin} ->
-			file:write_file("archivo_recibido", Bin),
+	case gen_tcp:recv(Sock,4) of
+		{ok, <<Size:32/integer-big>>} ->
+			recibir_tam(Sock,Size, <<>>),
 			io:format("Archivo recibido.~n");
-		{error, Reason} ->
-			io:format("Error al recibir : ~p~n", [Reason])
+		_ ->
+			io:format("Error al recibir Tamanio~n")
 	end.
+
+recibir_tam(_Sock,0,Acum) ->
+	file:write_file("archivo_recibido",Acum),
+	io:format("Archivo recibido completo [~p bytes]~n", [byte_size(Acum)]);
+recibir_tam(Sock,Restantes,Acum) ->
+	to_read = Restantes,
+	case gen_tcp:recv(Sock, to_read) of
+		{ok, Datos} ->
+			recibir_tam(Sock, Restantes - byte_size(Datos), <<Acum/binary, Datos/binary>>);
+		{error, Reason} ->
+			io:fwrite("Error al recibir ~p~n", [Reason])
+		end.
+
+
 
 enviar(IP, Puerto, RutaArchivo) ->
 	{ok,Bin} = file:read_file(RutaArchivo),
+	Size = byte_size(Bin),
 	{ok,Sock} = gen_tcp:connect(IP, Puerto, [binary, {packet, 0}, {active, false}]),
+	gen_tcp:send(Sock, <<Size:32/integer-big>>),
+	io:format("Tamano enviado~n"),
 	ok = gen_tcp:send(Sock,Bin),
 	io:format("Archivo enviado~n"),
 	gen_tcp:close(Sock).
